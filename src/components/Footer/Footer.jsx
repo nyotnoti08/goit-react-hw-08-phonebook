d82ead962c3caf073e42f7cf7c0d671ddff81922
nyotnoti08 <50961779+nyotnoti08@users.noreply.github.com>
2024-04-09 15:25:06 +0800

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <footer>
-      &copy; {new Date().getFullYear()} goit-react-hw-08-phonebook
+      nyotnoti08 &copy; {new Date().getFullYear()} goit-react-hw-08-phonebook
     </footer>
   );
 };
